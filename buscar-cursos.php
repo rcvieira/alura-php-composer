4e2c97443cee408375b1885ceae58805dfe44780
Rodrigo Cardoso Vieira <tr489@mg.trf1.gov.br>
2024-06-27 10:52:41 -0300

    Adiciona arquivo no bin

--- buscar-cursos.php
+++ buscar-cursos.php
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php
 
 require 'vendor/autoload.php';
@@ -14,5 +15,5 @@ $buscador = new Buscador($client, $crawler);
 $cursos = $buscador->buscar('/cursos-online-programacao/php');
 
 foreach ($cursos as $curso) {
-    echo exibeMensagem($curso);
+    exibeMensagem($curso);
 }
